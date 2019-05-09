11f845adcd16fd17d935eadf30a126ba700595ea
Jan Barlak <47349005+janbarlak@users.noreply.github.com>
2019-05-09 12:43:40 +0200

    Add files via upload

---
+++ bearhood.js
@@ -0,0 +1,8 @@
+
+/* Reviews link text changes color when mouseover to white */
+$("#reviews").mouseover(function(){$(this).css("color", "white");
+});
+
+$("#reviews").mouseleave(function(){$(this).css("color", "gold");
+});
+
